85588057e27f52a2621b62b8b52dc98e4b89d09c
nmtuyen <nguyenmanhtuyen.haui@gmail.com>
2021-05-04 16:06:50 +0700

    thi thuc hanh

---
+++ case study/Thi thực hành/Animal.js
@@ -0,0 +1,22 @@
+class Animal {
+    constructor(name, weight) {
+        this.name = name;
+        this.weigth = weight;
+    }
+    setName = function (a) {
+        this.name = a;
+    }
+    setWeight = function (b) {
+        this.weight = b;
+    }
+    getName = function () {
+        this.name = name;
+    }
+    getWeight = function () {
+        this.weight = weight;
+    }
+    toString = function () {
+        let str =
+        
+    }
+}
